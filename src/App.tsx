7fd2e38321130f4b8c3755038f59d5464b5613d7
Roma <elusiuve15.09.93@icloud.com>
2022-11-13 23:41:55 +0200

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {JobListPage} from "./components/JobListPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AppRootStateType, useAppDispatch} from "./bll/store";
+import {fetchJobListThunk} from "./bll/jobSlice";
+import {SinglePage} from "./components/SinglePage";
+import {useSelector} from "react-redux";
+import {RequestStatusType} from "./bll/appSlice";
+import CircularProgress from "@mui/material/CircularProgress";
+import {ErrorSnackBar} from "./components/ErrorSnackBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const status = useSelector<AppRootStateType, RequestStatusType>(state => state.appSlice.status)
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        dispatch(fetchJobListThunk())
+    }, [dispatch])
+
+    if (status === "loading") {
+        return <div className="flex justify-center my-72"><CircularProgress/></div>
+    }
+    //sdad
+    return (
+        <div>
+            <ErrorSnackBar/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"*"} element={<JobListPage/>}/>
+                    <Route path={":id"} element={<SinglePage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
